011acd258904106619d8ffd12fca4b7d606100f6
inmax <inmaculadaminarroruiz@gmail.com>
2020-05-25 00:12:53 +0200

    component structure

---
+++ src/components/Album/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+const Album = (props)=>{
+ return <div>Album</div>
+}
+export default Album;
---
+++ src/components/Cover/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Cover = props => {
+  return (
+    <div>
+      <header className="App-header" >Cover</header>
+    </div>
+  );
+};
+
+export default Cover;
