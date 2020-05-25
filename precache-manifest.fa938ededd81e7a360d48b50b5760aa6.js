1ada2d5bdb48d3a51b360e20ee035fcbd2b4b558
inmax <inmaculadaminarroruiz@gmail.com>
2020-05-25 07:38:18 +0200

    Updates

---
+++ precache-manifest.fa938ededd81e7a360d48b50b5760aa6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd40af06a0e74423e6acbc6d254159b",
+    "url": "/inmax/pizza-lover/index.html"
+  },
+  {
+    "revision": "3bf45944e214d64d3982",
+    "url": "/inmax/pizza-lover/static/css/3.160933d5.chunk.css"
+  },
+  {
+    "revision": "848aa97cb3f59be3d43a",
+    "url": "/inmax/pizza-lover/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef71bb8519970a6787dc",
+    "url": "/inmax/pizza-lover/static/js/2.d46c8e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inmax/pizza-lover/static/js/2.d46c8e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf45944e214d64d3982",
+    "url": "/inmax/pizza-lover/static/js/3.670add42.chunk.js"
+  },
+  {
+    "revision": "848aa97cb3f59be3d43a",
+    "url": "/inmax/pizza-lover/static/js/main.3e685e83.chunk.js"
+  },
+  {
+    "revision": "b3194a7a195bb1cb13e5",
+    "url": "/inmax/pizza-lover/static/js/runtime-main.866cf2ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inmax/pizza-lover/static/media/logo.5d5d9eef.svg"
+  }
+]);
